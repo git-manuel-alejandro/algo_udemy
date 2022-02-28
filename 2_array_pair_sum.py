1124fc87717ba92f6ab583f8a8aaedd44d2600d8
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-28 18:05:03 -0300

    2_array_pair_sum.py

--- 2_array_pair_sum.py
+++ 2_array_pair_sum.py
@@ -2,9 +2,18 @@ array = [1, 3, 2, 2]
 target = 4
 
 
-def pair_sum(array, target):
-    results = []
-    for i in array:
+def pair_sum(array, k):
+    seen = set()
+    output = set()
+
+    for num in array:
+        target = k - num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num, target), max(num, target)))
+
+    print('\n' .join(map(str, list(output))))
 
 
 pair_sum(array, target)
